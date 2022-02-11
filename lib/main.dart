6b97d485a4c640f328d58d4765a8f94497b6ba24
Sherajul-Shakil <sherajulshakil14@cse.pstu.ac.bd>
2022-02-12 00:17:00 +0600

    Social media post and profile complete UI

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:social_media_ui/screens/login_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Social UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
